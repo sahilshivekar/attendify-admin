1a32fa52243cf31580f8bcf9ebd9171c3a0d4bb7
sahilshivekar <sahilshivekarce@gmail.com>
2025-01-25 19:29:32 +0530

    Add splash screen

--- app/src/main/java/com/edu/wiet_admin/MainActivity.kt
+++ app/src/main/java/com/edu/wiet_admin/MainActivity.kt
@@ -11,12 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.edu.wiet_admin.ui.theme.WietadminTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        installSplashScreen()
         setContent {
             WietadminTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
